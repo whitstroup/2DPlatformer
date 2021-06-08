9a4d6afd3cb81a58c10ac291b0b9d6c244caafc4
Whit Stroup <email@example.com>
2021-06-08 13:51:56 -0500

    creating repo from scratch

---
+++ Platformer2D/Platformer2D.DesktopGL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Platformer2D.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var game = new PlatformerGame();
+
+            game.Run();
+        }
+    }
+}
